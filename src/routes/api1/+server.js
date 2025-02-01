070290084b09f6951e7c5233c694ec822189168c
HYEJIN <imagine@kookmin.ac.kr>
2025-02-02 02:15:51 +0900

    Remove: 불필요한 파일 제거

--- src/routes/api1/+server.js
+++
@@ -1,7 +0,0 @@
-export async function GET({fetch, request}){
-    return new Response(JSON.stringify({name:'kim'}))
-};
-
-export async function POST({fetch, request}){
-    return new Response(JSON.stringify({name:'kim'}))
-};
